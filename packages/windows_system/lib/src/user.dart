5e2a1bd33438a0db5b28a7674427756e6f503bd8
Halil İbrahim Durmuş <durmushalil@proton.me>
2023-01-07 13:41:04 +0300

    Add `windows_system` package

---
+++ packages/windows_system/lib/src/user.dart
@@ -0,0 +1,6 @@
+import 'package:win32/win32.dart';
+
+// TODO: Generate this class
+class User extends IInspectable {
+  User.fromRawPointer(super.ptr);
+}
---
+++ packages/windows_system/lib/windows_system.dart
@@ -0,0 +1,7 @@
+// ignore_for_file: directives_ordering
+
+library windows_system;
+
+export 'src/user.dart';
+
+export 'src/power/enums.g.dart';
