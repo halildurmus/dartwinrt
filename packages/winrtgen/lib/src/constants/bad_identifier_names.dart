a72651d0e0c3509ffc145ace00661faef9b1df22
Halil İbrahim Durmuş <durmushalil@proton.me>
2023-05-07 15:14:01 +0300

    Update license

--- packages/winrtgen/lib/src/constants/bad_identifier_names.dart
+++ packages/winrtgen/lib/src/constants/bad_identifier_names.dart
@@ -1,6 +1,6 @@
-// Copyright (c) 2023, the dartwinrt authors. Please see the AUTHORS file for
-// details. All rights reserved. Use of this source code is governed by a
-// BSD-style license that can be found in the LICENSE file.
+// Copyright (c) 2023, Dart | Windows. Please see the AUTHORS file for details.
+// All rights reserved. Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
 
 /// A list of all words that should not be used as identifiers.
 const badIdentifierNames = <String>{..._dartReservedWords, ..._dartTypes};
--- packages/winrtgen/lib/src/constants/headers.dart
+++ packages/winrtgen/lib/src/constants/headers.dart
@@ -1,11 +1,11 @@
-// Copyright (c) 2023, the dartwinrt authors. Please see the AUTHORS file for
-// details. All rights reserved. Use of this source code is governed by a
-// BSD-style license that can be found in the LICENSE file.
+// Copyright (c) 2023, Dart | Windows. Please see the AUTHORS file for details.
+// All rights reserved. Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
 
 const copyrightHeader = '''
-// Copyright (c) 2023, the dartwinrt authors. Please see the AUTHORS file for
-// details. All rights reserved. Use of this source code is governed by a
-// BSD-style license that can be found in the LICENSE file.
+// Copyright (c) 2023, Dart | Windows. Please see the AUTHORS file for details.
+// All rights reserved. Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
 ''';
 
 const classFileHeader = '''
