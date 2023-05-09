7520a66525908585142d734dc66a999e7bf2c881
Halil İbrahim Durmuş <durmushalil@proton.me>
2023-05-09 11:51:46 +0300

    Delete gamepad.dart

--- packages/windows_gaming/example/gamepad.dart
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2023, Dart | Windows. Please see the AUTHORS file for details.
-// All rights reserved. Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
-import 'package:windows_gaming/windows_gaming.dart';
-
-void main() {
-  final gamepads = Gamepad.gamepads;
-}
