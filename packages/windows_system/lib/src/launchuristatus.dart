7e8322e06db8d31c1fc1c3f435fce23a4ef1c693
Halil Durmus <durmushalil@proton.me>
2023-06-01 14:18:27 +0300

    feat(windows_system): add some enums required for `Launcher` APIs

---
+++ packages/windows_system/lib/src/launchuristatus.dart
@@ -0,0 +1,29 @@
+// Copyright (c) 2023, Dart | Windows. Please see the AUTHORS file for details.
+// All rights reserved. Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// THIS FILE IS GENERATED AUTOMATICALLY AND SHOULD NOT BE EDITED DIRECTLY.
+
+// ignore_for_file: deprecated_member_use_from_same_package
+
+import 'package:windows_foundation/windows_foundation.dart';
+
+/// Specifies the result of activating an application for a URI.
+///
+/// {@category enum}
+enum LaunchUriStatus implements WinRTEnum {
+  success(0),
+  appUnavailable(1),
+  protocolUnavailable(2),
+  unknown(3);
+
+  @override
+  final int value;
+
+  const LaunchUriStatus(this.value);
+
+  factory LaunchUriStatus.from(int value) =>
+      LaunchUriStatus.values.firstWhere((e) => e.value == value,
+          orElse: () => throw ArgumentError.value(
+              value, 'value', 'No enum value with that value'));
+}
---
+++ packages/windows_system/lib/src/processorarchitecture.dart
@@ -0,0 +1,32 @@
+// Copyright (c) 2023, Dart | Windows. Please see the AUTHORS file for details.
+// All rights reserved. Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// THIS FILE IS GENERATED AUTOMATICALLY AND SHOULD NOT BE EDITED DIRECTLY.
+
+// ignore_for_file: deprecated_member_use_from_same_package
+
+import 'package:windows_foundation/windows_foundation.dart';
+
+/// Specifies the processor architecture supported by an app.
+///
+/// {@category enum}
+enum ProcessorArchitecture implements WinRTEnum {
+  x86(0),
+  arm(5),
+  x64(9),
+  neutral(11),
+  arm64(12),
+  x86OnArm64(14),
+  unknown(65535);
+
+  @override
+  final int value;
+
+  const ProcessorArchitecture(this.value);
+
+  factory ProcessorArchitecture.from(int value) =>
+      ProcessorArchitecture.values.firstWhere((e) => e.value == value,
+          orElse: () => throw ArgumentError.value(
+              value, 'value', 'No enum value with that value'));
+}
