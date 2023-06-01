4668bcb2c09f899fe5b4af6d3563a9c178399fbe
Halil Durmus <durmushalil@proton.me>
2023-06-01 13:49:36 +0300

    chore(windows_ui): add example of sending a toast notification (#220)

---
+++ packages/windows_ui/example/notification.dart
@@ -0,0 +1,32 @@
+// Copyright (c) 2023, Dart | Windows. Please see the AUTHORS file for details.
+// All rights reserved. Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+import 'package:windows_ui/windows_ui.dart';
+
+void main() async {
+  const applicationId = 'dartwinrt';
+  // Create a toast notifier
+  final toastNotifier =
+      ToastNotificationManager.createToastNotifierWithId(applicationId);
+  if (toastNotifier != null) {
+    final toastContent = ToastNotificationManager.getTemplateContent(
+        ToastTemplateType.toastText02);
+    if (toastContent != null) {
+      final xmlNodeList = toastContent.getElementsByTagName('text');
+      const title = 'Updates available';
+      const content = 'New updates have been found for this program.';
+      // Set the title on the toast notification
+      xmlNodeList.item(0)?.appendChild(toastContent.createTextNode(title));
+      // Set the content on the toast notification
+      xmlNodeList.item(1)?.appendChild(toastContent.createTextNode(content));
+
+      // Create a toast notification
+      final toastNotification =
+          ToastNotification.createToastNotification(toastContent);
+
+      // Show the toast notification
+      toastNotifier.show(toastNotification);
+    }
+  }
+}
