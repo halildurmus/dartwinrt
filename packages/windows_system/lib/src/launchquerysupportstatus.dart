b229f83d6ee56102628667564fd2540f53cec5e1
Halil Durmus <durmushalil@proton.me>
2023-06-01 14:23:19 +0300

    feat(windows_system): add some enums required for `Launcher` APIs (#224)

---
+++ packages/windows_system/lib/src/launchquerysupportstatus.dart
@@ -0,0 +1,30 @@
+// Copyright (c) 2023, Dart | Windows. Please see the AUTHORS file for details.
+// All rights reserved. Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// THIS FILE IS GENERATED AUTOMATICALLY AND SHOULD NOT BE EDITED DIRECTLY.
+
+// ignore_for_file: deprecated_member_use_from_same_package
+
+import 'package:windows_foundation/windows_foundation.dart';
+
+/// Specifies whether an app is available that supports activation.
+///
+/// {@category enum}
+enum LaunchQuerySupportStatus implements WinRTEnum {
+  available(0),
+  appNotInstalled(1),
+  appUnavailable(2),
+  notSupported(3),
+  unknown(4);
+
+  @override
+  final int value;
+
+  const LaunchQuerySupportStatus(this.value);
+
+  factory LaunchQuerySupportStatus.from(int value) =>
+      LaunchQuerySupportStatus.values.firstWhere((e) => e.value == value,
+          orElse: () => throw ArgumentError.value(
+              value, 'value', 'No enum value with that value'));
+}
