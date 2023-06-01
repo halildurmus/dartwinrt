8bc1d73532d6e5d23e732b3946c4eb66de0ad6a5
Halil Durmus <durmushalil@proton.me>
2023-06-01 21:27:38 +0300

    chore(windows_system): add example of calling `Launcher` APIs

--- packages/windows_system/example/README.md
+++ packages/windows_system/example/README.md
@@ -2,6 +2,8 @@
 
 | Example              | Description                                             |
 | -------------------- | ------------------------------------------------------- |
+| [launcher.dart]      | Demonstrates calling Launcher APIs                      |
 | [power_manager.dart] | Demonstrates retrieving current power state information |
 
+[launcher.dart]: https://github.com/dart-windows/dartwinrt/blob/main/packages/windows_system/example/launcher.dart
 [power_manager.dart]: https://github.com/dart-windows/dartwinrt/blob/main/packages/windows_system/example/power_manager.dart
---
+++ packages/windows_system/example/launcher.dart
@@ -0,0 +1,52 @@
+// Copyright (c) 2023, Dart | Windows. Please see the AUTHORS file for details.
+// All rights reserved. Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+import 'package:windows_storage/windows_storage.dart';
+import 'package:windows_system/windows_system.dart';
+
+Future<void> launchUri(Uri uri) async {
+  // Launch the URI and get the result of the operation
+  final isSuccess = await Launcher.launchUriAsync(uri);
+  if (isSuccess) {
+    print('Uri "$uri" successfully launched');
+  } else {
+    print('Uri "$uri" launch failed');
+  }
+}
+
+Future<void> launchFolderPath(String path) async {
+  // Launch the folder and get the result of the operation
+  final isSuccess = await Launcher.launchFolderPathAsync(path);
+  if (isSuccess) {
+    print('Folder "$path" successfully launched');
+  } else {
+    print('Folder "$path" launch failed');
+  }
+}
+
+void main() async {
+  // Launch the https://win32.pub page in the default browser
+  await launchUri(Uri.parse('https://win32.pub'));
+
+  // Launch the Windows Settings app
+  await launchUri(Uri.parse('ms-settings:'));
+
+  // Launch the Default Apps settings page
+  await launchUri(Uri.parse('ms-settings:defaultapps'));
+
+  // Get the file handlers for ".html" files
+  final handlers = await Launcher.findFileHandlersAsync('.html');
+  if (handlers.isNotEmpty) {
+    print('Found ${handlers.length} handler(s) for ".html" files:');
+    for (final handler in handlers) {
+      print(' - ${handler.displayInfo?.displayName}');
+    }
+  }
+
+  final path = UserDataPaths.getDefault()?.downloads;
+  if (path != null) {
+    // Launch the Downloads folder
+    await launchFolderPath(path);
+  }
+}
