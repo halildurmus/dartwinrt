49b568642f3846be918b05bff2f9d9fb63c13356
Halil Durmus <durmushalil@proton.me>
2023-06-17 18:17:18 +0300

    feat(windows_media): add `VideoFrame` (#252)

---
+++ packages/windows_media/lib/src/imediaframe.dart
@@ -0,0 +1,280 @@
+// Copyright (c) 2023, Dart | Windows. Please see the AUTHORS file for details.
+// All rights reserved. Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// THIS FILE IS GENERATED AUTOMATICALLY AND SHOULD NOT BE EDITED DIRECTLY.
+
+// ignore_for_file: constant_identifier_names, non_constant_identifier_names
+// ignore_for_file: unnecessary_import, unused_import
+
+import 'dart:async';
+import 'dart:ffi';
+
+import 'package:ffi/ffi.dart';
+import 'package:win32/win32.dart' hide DocumentProperties;
+import 'package:windows_foundation/internal.dart';
+import 'package:windows_foundation/windows_foundation.dart';
+
+/// @nodoc
+const IID_IMediaFrame = '{bfb52f8c-5943-47d8-8e10-05308aa5fbd0}';
+
+class IMediaFrame extends IInspectable implements IClosable {
+  // vtable begins at 6, is 11 entries long.
+  IMediaFrame.fromPtr(super.ptr);
+
+  factory IMediaFrame.from(IInspectable interface) =>
+      IMediaFrame.fromPtr(interface.toInterface(IID_IMediaFrame));
+
+  String get type {
+    final retValuePtr = calloc<HSTRING>();
+
+    try {
+      final hr = ptr.ref.vtable
+          .elementAt(6)
+          .cast<
+              Pointer<
+                  NativeFunction<
+                      HRESULT Function(VTablePointer lpVtbl,
+                          Pointer<IntPtr> retValuePtr)>>>()
+          .value
+          .asFunction<
+              int Function(VTablePointer lpVtbl,
+                  Pointer<IntPtr> retValuePtr)>()(ptr.ref.lpVtbl, retValuePtr);
+
+      if (FAILED(hr)) throw WindowsException(hr);
+
+      return retValuePtr.toDartString();
+    } finally {
+      WindowsDeleteString(retValuePtr.value);
+      free(retValuePtr);
+    }
+  }
+
+  bool get isReadOnly {
+    final retValuePtr = calloc<Bool>();
+
+    try {
+      final hr = ptr.ref.vtable
+          .elementAt(7)
+          .cast<
+              Pointer<
+                  NativeFunction<
+                      HRESULT Function(
+                          VTablePointer lpVtbl, Pointer<Bool> retValuePtr)>>>()
+          .value
+          .asFunction<
+              int Function(VTablePointer lpVtbl,
+                  Pointer<Bool> retValuePtr)>()(ptr.ref.lpVtbl, retValuePtr);
+
+      if (FAILED(hr)) throw WindowsException(hr);
+
+      return retValuePtr.value;
+    } finally {
+      free(retValuePtr);
+    }
+  }
+
+  set relativeTime(Duration? value) {
+    final hr = ptr.ref.vtable
+            .elementAt(8)
+            .cast<
+                Pointer<
+                    NativeFunction<
+                        HRESULT Function(
+                            VTablePointer lpVtbl, VTablePointer value)>>>()
+            .value
+            .asFunction<
+                int Function(VTablePointer lpVtbl, VTablePointer value)>()(
+        ptr.ref.lpVtbl, value?.toReference().ptr.ref.lpVtbl ?? nullptr);
+
+    if (FAILED(hr)) throw WindowsException(hr);
+  }
+
+  Duration? get relativeTime {
+    final retValuePtr = calloc<COMObject>();
+
+    final hr = ptr.ref.vtable
+        .elementAt(9)
+        .cast<
+            Pointer<
+                NativeFunction<
+                    HRESULT Function(VTablePointer lpVtbl,
+                        Pointer<COMObject> retValuePtr)>>>()
+        .value
+        .asFunction<
+            int Function(VTablePointer lpVtbl,
+                Pointer<COMObject> retValuePtr)>()(ptr.ref.lpVtbl, retValuePtr);
+
+    if (FAILED(hr)) {
+      free(retValuePtr);
+      throw WindowsException(hr);
+    }
+
+    if (retValuePtr.ref.isNull) {
+      free(retValuePtr);
+      return null;
+    }
+
+    final reference = IReference<Duration?>.fromPtr(retValuePtr,
+        referenceIid: '{604d0c4c-91de-5c2a-935f-362f13eaf800}');
+    return reference.value;
+  }
+
+  set systemRelativeTime(Duration? value) {
+    final hr = ptr.ref.vtable
+            .elementAt(10)
+            .cast<
+                Pointer<
+                    NativeFunction<
+                        HRESULT Function(
+                            VTablePointer lpVtbl, VTablePointer value)>>>()
+            .value
+            .asFunction<
+                int Function(VTablePointer lpVtbl, VTablePointer value)>()(
+        ptr.ref.lpVtbl, value?.toReference().ptr.ref.lpVtbl ?? nullptr);
+
+    if (FAILED(hr)) throw WindowsException(hr);
+  }
+
+  Duration? get systemRelativeTime {
+    final retValuePtr = calloc<COMObject>();
+
+    final hr = ptr.ref.vtable
+        .elementAt(11)
+        .cast<
+            Pointer<
+                NativeFunction<
+                    HRESULT Function(VTablePointer lpVtbl,
+                        Pointer<COMObject> retValuePtr)>>>()
+        .value
+        .asFunction<
+            int Function(VTablePointer lpVtbl,
+                Pointer<COMObject> retValuePtr)>()(ptr.ref.lpVtbl, retValuePtr);
+
+    if (FAILED(hr)) {
+      free(retValuePtr);
+      throw WindowsException(hr);
+    }
+
+    if (retValuePtr.ref.isNull) {
+      free(retValuePtr);
+      return null;
+    }
+
+    final reference = IReference<Duration?>.fromPtr(retValuePtr,
+        referenceIid: '{604d0c4c-91de-5c2a-935f-362f13eaf800}');
+    return reference.value;
+  }
+
+  set duration(Duration? value) {
+    final hr = ptr.ref.vtable
+            .elementAt(12)
+            .cast<
+                Pointer<
+                    NativeFunction<
+                        HRESULT Function(
+                            VTablePointer lpVtbl, VTablePointer value)>>>()
+            .value
+            .asFunction<
+                int Function(VTablePointer lpVtbl, VTablePointer value)>()(
+        ptr.ref.lpVtbl, value?.toReference().ptr.ref.lpVtbl ?? nullptr);
+
+    if (FAILED(hr)) throw WindowsException(hr);
+  }
+
+  Duration? get duration {
+    final retValuePtr = calloc<COMObject>();
+
+    final hr = ptr.ref.vtable
+        .elementAt(13)
+        .cast<
+            Pointer<
+                NativeFunction<
+                    HRESULT Function(VTablePointer lpVtbl,
+                        Pointer<COMObject> retValuePtr)>>>()
+        .value
+        .asFunction<
+            int Function(VTablePointer lpVtbl,
+                Pointer<COMObject> retValuePtr)>()(ptr.ref.lpVtbl, retValuePtr);
+
+    if (FAILED(hr)) {
+      free(retValuePtr);
+      throw WindowsException(hr);
+    }
+
+    if (retValuePtr.ref.isNull) {
+      free(retValuePtr);
+      return null;
+    }
+
+    final reference = IReference<Duration?>.fromPtr(retValuePtr,
+        referenceIid: '{604d0c4c-91de-5c2a-935f-362f13eaf800}');
+    return reference.value;
+  }
+
+  set isDiscontinuous(bool value) {
+    final hr = ptr.ref.vtable
+            .elementAt(14)
+            .cast<
+                Pointer<
+                    NativeFunction<
+                        HRESULT Function(VTablePointer lpVtbl, Bool value)>>>()
+            .value
+            .asFunction<int Function(VTablePointer lpVtbl, bool value)>()(
+        ptr.ref.lpVtbl, value);
+
+    if (FAILED(hr)) throw WindowsException(hr);
+  }
+
+  bool get isDiscontinuous {
+    final retValuePtr = calloc<Bool>();
+
+    try {
+      final hr = ptr.ref.vtable
+          .elementAt(15)
+          .cast<
+              Pointer<
+                  NativeFunction<
+                      HRESULT Function(
+                          VTablePointer lpVtbl, Pointer<Bool> retValuePtr)>>>()
+          .value
+          .asFunction<
+              int Function(VTablePointer lpVtbl,
+                  Pointer<Bool> retValuePtr)>()(ptr.ref.lpVtbl, retValuePtr);
+
+      if (FAILED(hr)) throw WindowsException(hr);
+
+      return retValuePtr.value;
+    } finally {
+      free(retValuePtr);
+    }
+  }
+
+  IPropertySet get extendedProperties {
+    final retValuePtr = calloc<COMObject>();
+
+    final hr = ptr.ref.vtable
+        .elementAt(16)
+        .cast<
+            Pointer<
+                NativeFunction<
+                    HRESULT Function(VTablePointer lpVtbl,
+                        Pointer<COMObject> retValuePtr)>>>()
+        .value
+        .asFunction<
+            int Function(VTablePointer lpVtbl,
+                Pointer<COMObject> retValuePtr)>()(ptr.ref.lpVtbl, retValuePtr);
+
+    if (FAILED(hr)) {
+      free(retValuePtr);
+      throw WindowsException(hr);
+    }
+
+    return IPropertySet.fromPtr(retValuePtr);
+  }
+
+  late final _iClosable = IClosable.from(this);
+
+  @override
+  void close() => _iClosable.close();
+}
