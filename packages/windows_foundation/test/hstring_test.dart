d8e3533b76a41d35a9a8166e1ae1ba9c373f62e0
Halil Durmus <durmushalil@proton.me>
2023-07-23 12:33:25 +0300

    feat(windows_foundation): add wrapper for `HSTRING` (#324)

---
+++ packages/windows_foundation/test/hstring_test.dart
@@ -0,0 +1,101 @@
+// Copyright (c) 2023, Dart | Windows. Please see the AUTHORS file for details.
+// All rights reserved. Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+@TestOn('windows')
+
+import 'package:test/test.dart';
+import 'package:win32/win32.dart';
+import 'package:windows_foundation/windows_foundation.dart';
+
+void main() {
+  if (!isWindowsRuntimeAvailable()) {
+    print('Skipping tests because Windows Runtime is not available.');
+    return;
+  }
+
+  const testRuns = 100;
+
+  group('HString', () {
+    test('allocation', () {
+      const testString = 'Hello world!';
+      for (var i = 0; i < testRuns; i++) {
+        final hString = HString.fromString(testString);
+        expect(hString.toString(), equals(testString));
+        hString.free();
+      }
+    });
+
+    test('allocation of long strings', () {
+      final longString = 'A very long string with padding.' * 65536;
+      // Ten allocations is probably enough for an expensive test like this.
+      for (var i = 0; i < 10; i++) {
+        final hString = HString.fromString(longString);
+        expect(hString.toString(), equals(longString));
+        hString.free();
+      }
+    });
+
+    test('clone', () {
+      const testString1 = 'This message is not unique.';
+      const testString2 = '';
+      for (var i = 0; i < testRuns; i++) {
+        for (final str in [testString1, testString2]) {
+          final original = HString.fromString(str);
+          final clone = original.clone();
+          expect(original.toString(), equals(clone.toString()));
+          expect(original.handle, equals(clone.handle));
+          clone.free();
+          original.free();
+        }
+      }
+    });
+
+    test('concatenation', () {
+      for (var i = 0; i < testRuns; i++) {
+        final first = HString.fromString('Windows');
+        final second = HString.fromString(' and Dart');
+        final matchInHeaven = first + second;
+        expect(matchInHeaven.toString(), equals('Windows and Dart'));
+        [first, second, matchInHeaven].forEach((object) => object.free());
+      }
+    });
+
+    test('empty', () {
+      for (var i = 0; i < testRuns; i++) {
+        final hString = const HString.empty();
+        expect(hString.isEmpty, isTrue);
+        expect(hString.toString(), isEmpty);
+        hString.free();
+      }
+    });
+
+    test('isEmpty', () {
+      const testString = 'dartwinrt';
+      for (var i = 0; i < testRuns; i++) {
+        final hString1 = HString.fromString(testString);
+        expect(hString1.isEmpty, isFalse);
+        final hString2 = const HString.empty();
+        expect(hString2.isEmpty, isTrue);
+        final hString3 = HString.fromString('');
+        expect(hString3.isEmpty, isTrue);
+        hString1.free();
+        hString2.free();
+        hString3.free();
+      }
+    });
+
+    test('length', () {
+      const testString = 'Lorem ipsum dolor sit amet, consectetur adipiscing '
+          'elit, sed do eiusmod tempor incididunt ut labore et dolore magna '
+          'aliqua.';
+      for (var i = 0; i < testRuns; i++) {
+        final hString = HString.fromString(testString);
+        expect(testString.length, equals(123));
+        expect(hString.length, equals(123));
+        expect(hString.toString(), equals(testString));
+        hString.free();
+      }
+    });
+  });
+}
